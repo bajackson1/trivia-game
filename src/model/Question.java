c77ea2be435daf5800ba8deeeb6efb8614043ce8
piecon18 <piecon1805@gmail.com>
2025-03-29 21:21:34 -0400

    bug fix 6

--- src/model/Question.java
+++ src/model/Question.java
@@ -10,7 +10,7 @@ public class Question implements Serializable {
     private String questionText;
     private String[] options;
     private char correctAnswer;
-    private int questionNumber = 0;
+    private int questionNumber;
 
     // Added by Brooks
     public Question(String questionText, String[] options, char correctAnswer, int questionNumber) {
